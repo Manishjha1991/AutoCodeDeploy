2ac878b8eb453a206200fcb2c2448ed29995fe99
Manish <mkjha9555@gmail.com>
2017-11-26 18:59:36 +0530

    fix issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,12 +7,12 @@ module.exports = {
   }],
   deploy: {
     production: {
-      user: 'node',
-      host: 'ec2-user@ec2-13-127-17-170.ap-south-1.compute.amazonaws.com',
+      user: 'ec2-user',
+      host: 'ec2-13-127-17-170.ap-south-1.compute.amazonaws.com',
       key: 'codeDeployTest.pem',
       ref: 'origin/master',
       repo: 'git@github.com:Manishjha1991/AutoCodeDeploy',
-      path: './Users/manish/Desktop/AutoCodeDeploy',
+      path: '~/AutoCodeDeploy',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
