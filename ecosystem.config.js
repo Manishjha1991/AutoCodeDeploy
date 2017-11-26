72acdbe1e61271fc9b21db917228e8a9badbbb3a
Manish <mkjha9555@gmail.com>
2017-11-26 18:09:23 +0530

    setUp Pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+
+
+module.exports = {
+  apps: [{
+    name: 'codeDeploy',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'node',
+      host: 'http://ec2-13-127-17-170.ap-south-1.compute.amazonaws.com:3000',
+      key: 'codeDeployTest.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:Manishjha1991/AutoCodeDeploy',
+      path: './Users/manish/Desktop/AutoCodeDeploy',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
