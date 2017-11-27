4183543562e3691e92f0b954aef7e86705d5cd44
Manish <mkjha9555@gmail.com>
2017-11-27 13:48:54 +0530

    test

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const express = require('express')
 const app = express()
-
 app.use(express.static('public'))
-// app.get('/', (req, res) => {
-//   res.send('HEY!')
-// })
 app.listen(3000, () => console.log('Server running on port 3000'))
